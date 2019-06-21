b11af324106781a5bb7cef3392c9204dbda3be15
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-06-21 09:31:12 -0600

    App component: setup component state, mapStateToProps, and connect on export

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -7,9 +9,18 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  state = {
+    newSmurf: {
+      name: '',
+      age: null,
+      height: ''
+    }
+  };
+
+  componentDidMount() {}
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
@@ -19,4 +30,13 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs,
+  fetchingSmurfs: state.fetchingSmurfs,
+  error: state.error
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
